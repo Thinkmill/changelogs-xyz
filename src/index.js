e5bd300eac2760284616ea73257a887088f888a9
Ben Conolly <ben@thinkmill.com.au>
2020-03-03 17:50:03 +1100

    better

--- src/index.js
+++ src/index.js
@@ -1,21 +1,36 @@
-import React, { useState, useEffect } from "react";
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+import { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
+import ReactMarkdown from "react-markdown";
+
 import "./index.css";
 import "./App.css";
 
-async function getChangelog() {
-  let packageName = window.location.pathname.substr(1);
-  return fetch(`https://unpkg.com/${packageName}/CHANGELOG.md`).then(response =>
-    response.text()
+async function getChangelog(packageName) {
+  // Watch this space - this function is going to get way more complicated, trust me
+  return fetch(`https://unpkg.com/${packageName}/CHANGELOG.md`).then(
+    response => {
+      console.log("🕸", response);
+      return response.text();
+    }
   );
 }
 
+const statusMessage = {
+  loading: () => "fetching the changelog for you",
+  loaded: () => "",
+  failed: () =>
+    "something went wrong loading this! please raise an issue including the url you typed at https://github.com/thinkmill/changelog-xyz"
+};
+
 function App() {
+  const packageName = window.location.pathname.substr(1);
   const [changelog, updateChangelog] = useState(null);
   const [changelogStatus, updateStatus] = useState("loading");
 
   useEffect(() => {
-    getChangelog()
+    getChangelog(packageName)
       .then(text => {
         updateChangelog(text);
         updateStatus("loaded");
@@ -26,12 +41,28 @@ function App() {
       });
   });
 
+  const Message = statusMessage[changelogStatus];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>{changelogStatus}</h2>
-        <p>{changelog}</p>
-      </header>
+    <div
+      css={{
+        padding: "24px"
+      }}
+    >
+      <div>
+        <h1 css={{ textAlign: "center" }}>XYZ changelog: {packageName}</h1>
+        <h2 css={{ textAlign: "center" }}>
+          <Message />
+        </h2>
+        <div
+          css={{
+            margin: "0 auto",
+            maxWidth: "680px"
+          }}
+        >
+          <ReactMarkdown source={changelog} />
+        </div>
+      </div>
     </div>
   );
 }
