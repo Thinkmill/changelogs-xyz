f96c69390ae7237d04355e3edb20a71614795e68
Ben Conolly <ben@thinkmill.com.au>
2020-03-03 18:31:34 +1100

    straight to master

--- src/index.js
+++ src/index.js
@@ -9,39 +9,98 @@ import "./App.css";
 
 async function getChangelog(packageName) {
   // Watch this space - this function is going to get way more complicated, trust me
+
+  // TODO do this better thing instead
+  // https://unpkg.com/@changesets/cli@2.5.2/?meta
+
   return fetch(`https://unpkg.com/${packageName}/CHANGELOG.md`).then(
     response => {
-      console.log("🕸", response);
+      if (response.status !== 200) throw response;
       return response.text();
     }
   );
 }
 
-const statusMessage = {
-  loading: () => "fetching the changelog for you",
-  loaded: () => "",
-  failed: () =>
-    "something went wrong loading this! please raise an issue including the url you typed at https://github.com/thinkmill/changelog-xyz"
+const FailWhale = () => (
+  <h2 css={{ textAlign: "center" }}>
+    something went wrong loading this!{" "}
+    <a href="https://github.com/Thinkmill/changelog-xyz/issues">
+      please raise an issue
+    </a>{" "}
+    including the full url
+  </h2>
+);
+
+const ErrorMessage = ({ type, text, nameAndVersion }) => {
+  if (type === "text") {
+    return text;
+  }
+
+  if (type === "filenotfound") {
+    return (
+      <div>
+        <p>
+          We couldn't resolve the file. It may be we don't support the format
+          yet.
+        </p>
+        <p>
+          You can check over on{" "}
+          <a href={`https://unpkg.com/browse/${nameAndVersion}/`}>unpkg</a> to
+          see if we missed it
+        </p>
+        <p>
+          If we did -{" "}
+          <a href="https://github.com/Thinkmill/changelog-xyz/issues">
+            please raise an issue
+          </a>{" "}
+        </p>
+      </div>
+    );
+  }
+  console.error("things are exteremely wrong");
+  return "This is a completely unknown error";
 };
 
 function App() {
   const packageName = window.location.pathname.substr(1);
   const [changelog, updateChangelog] = useState(null);
-  const [changelogStatus, updateStatus] = useState("loading");
+  const [isLoading, setLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState();
 
   useEffect(() => {
     getChangelog(packageName)
       .then(text => {
         updateChangelog(text);
-        updateStatus("loaded");
+        setLoading(false);
       })
       .catch(err => {
-        console.log(err);
-        updateStatus("failed");
-      });
-  });
+        setLoading(false);
 
-  const Message = statusMessage[changelogStatus];
+        return err.text().then(message => {
+          /*
+            error text to handle:
+                `Cannot find package...`
+                `Cannot find "FILENAME" in pkg@version`
+          */
+
+          if (!message) {
+            setErrorMessage({
+              type: "text",
+              text: "This is a completely unknown error"
+            });
+          }
+          // This happens if there is a missing package
+          else if (message.startsWith('Cannot find "')) {
+            setErrorMessage({
+              type: "filenotfound",
+              nameAndVersion: message.split(" in ")[1]
+            });
+          } else {
+            setErrorMessage(message);
+          }
+        });
+      });
+  }, [packageName]);
 
   return (
     <div
@@ -51,9 +110,20 @@ function App() {
     >
       <div>
         <h1 css={{ textAlign: "center" }}>XYZ changelog: {packageName}</h1>
-        <h2 css={{ textAlign: "center" }}>
-          <Message />
-        </h2>
+        <p>
+          Thanks for using changelog.xyz! Just add a package name to the URL and
+          we'll (try to) show you its changelog!
+        </p>
+        <p>We are very in beta right now, </p>
+        {isLoading && (
+          <h2 css={{ textAlign: "center" }}>fetching the changelog for you</h2>
+        )}
+        {errorMessage && (
+          <div>
+            <FailWhale />
+            <ErrorMessage {...errorMessage} />
+          </div>
+        )}
         <div
           css={{
             margin: "0 auto",
